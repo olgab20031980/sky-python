88dd45506020c10dc342397945ae0117c7aa9309
olgab20031980@gmail.com <olgab20031980@gmail.com>
2026-07-01 14:26:47 +0300

    Fix lesson_05

--- lesson_05/tests/test_lesson05_task1.py
+++ lesson_05/tests/test_lesson05_task1.py
@@ -1,25 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_navigation():
     driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
 
     driver.get("https://httpbin.org/")
 
-    link = driver.find_element(By.LINK_TEXT, "HTML Form")
+    link = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='/forms/post']"))
+    )
     link.click()
 
+    wait.until(EC.url_contains("/forms/post"))
     current_url = driver.current_url
     assert "/forms/post" in current_url, (
         f"Expected URL to contain '/forms/post', but got {current_url}"
-        )
+    )
 
     driver.back()
 
+    wait.until(EC.url_contains("httpbin.org"))
     current_url = driver.current_url
     assert current_url == "https://httpbin.org/", (
         f"Expected URL to be 'https://httpbin.org/', but got {current_url}"
-        )
+    )
 
     driver.quit()
--- lesson_05/tests/test_lesson05_task2.py
+++ lesson_05/tests/test_lesson05_task2.py
@@ -1,22 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_form_submission():
     driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
+
     driver.get("https://httpbin.org/forms/post")
 
     name_input = driver.find_element(By.NAME, "custname")
-
     name_input.send_keys("SkyPro Student")
 
-    submit_button = driver.find_element
-    (By.XPATH, "//button[text()='Submit']")
+    submit_button = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    )
     submit_button.click()
 
+    wait.until(EC.url_contains("/post"))
     current_url = driver.current_url
     assert "/post" in current_url, (
         f"Expected URL to contain '/post', but got {current_url}"
-        )
+    )
 
     driver.quit()
